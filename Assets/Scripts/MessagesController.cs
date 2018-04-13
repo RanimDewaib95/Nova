4de231fa1d3dcb3f545de6b115263920c66359f1
Raneem Dewaib <r.dewaib95@gmail.com>
2018-04-14 00:32:00 +0200

    Add project

---
+++ Assets/Scripts/MessagesController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MessagesController : MonoBehaviour {
+    Queue<string> messages = new Queue<string>();
+
+    GameObject button;
+
+    Text message1;
+    string message2;
+    string message3, message4;
+
+    // Use this for initialization
+    void Start () {
+        button = GameObject.Find("Message 1");
+
+        message2 = "ranim is 2";
+        message3 = "heba is 3";
+        message4 = "heba is 4";
+        //message2 = GameObject.Find("text2").GetComponentInChildren<Text>();
+        //message3 = GameObject.Find("text3").GetComponentInChildren<Text>();
+
+        //message2.SetActive(false);
+        //message3.SetActive(false);
+
+        messages.Enqueue(message2);
+        messages.Enqueue(message3);
+        messages.Enqueue(message4);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        button.GetComponent<Button>().onClick.AddListener(delegate { updateButton(); });
+	}
+
+    void updateButton()
+    {
+        if (messages.Count != 0)
+        {
+            button.GetComponentInChildren<Text>().text = messages.Peek().ToString();
+            messages.Dequeue();
+            Debug.Log("changed");
+        }
+    }
+}
---
+++ Assets/Scripts/Player Controller/MovePlayer.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public float moveSpeed = 50f;//Change in inspector to adjust move speed
+    public int flag = 0;
+    Vector3 forward, right; // Keeps track of our relative forward and right vectors
+
+    void Start()
+    {
+        //forward = Camera.main.transform.forward; // Set forward to equal the camera's forward vector
+        forward = transform.forward;
+        forward.y = 0; // make sure y is 0
+        forward = Vector3.Normalize(forward); // make sure the length of vector is set to a max of 1.0
+        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward; // set the right-facing vector to be facing right relative to the camera's forward vector
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) && flag == 0)
+        {
+            flag = 1;
+            Move();
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) && flag == 0)
+        {
+            flag = 1;
+            Move();
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            StartCoroutine(RotateAround(Vector3.up, 90.0f, 1.0f));
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            StartCoroutine(RotateAround(Vector3.up, -90.0f, 1.0f));
+        }
+    }
+
+    void Move()
+    {
+
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        Vector3 HorStep = new Vector3(100f, 0f, 0f);
+        Vector3 rightMovement = (HorStep + right) * Input.GetAxis("Horizontal");
+
+        Vector3 VerStep = new Vector3(0f, 0f, 100f);
+        Vector3 upMovement = (VerStep + forward) * Input.GetAxis("Vertical");
+
+        Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
+
+        transform.forward = heading;
+        transform.position += rightMovement;
+        transform.position += upMovement;
+    }
+
+    IEnumerator RotateAround(Vector3 axis, float angle, float duration)
+    {
+        float elapsed = 0.0f;
+        float rotated = 0.0f;
+        while (elapsed < duration)
+        {
+            float step = angle / duration * Time.deltaTime;
+            transform.RotateAround(transform.position, axis, step);
+            elapsed += Time.deltaTime;
+            rotated += step;
+            yield return null;
+        }
+        transform.RotateAround(transform.position, axis, angle - rotated);
+    }
+}
