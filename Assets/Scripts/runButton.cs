3077a90f795899e6ee2edd70c385f3fa05d84f2e
Raneem Dewaib <r.dewaib95@gmail.com>
2018-04-17 12:39:15 +0200

    Level 1 Trail

--- Assets/Scripts/runButton.cs
+++ Assets/Scripts/runButton.cs
@@ -9,31 +9,5 @@ public class runButton : MonoBehaviour{
     public void runButtonClicked()
     {
         clicked = true;
-        /*MovePlayer script = FindObjectOfType<MovePlayer>();
-        string blockName;
-
-        for(int i = 0; i < DragHandler.chosenBlocks.Count; i++)
-        {
-            Debug.Log(i);
-            blockName = DragHandler.chosenBlocks[i];
-            switch(blockName)
-            {
-                case "rotateRightBlock(Clone)":
-                    StartCoroutine(script.RotateAround(Vector3.up, 90.0f, 1.0f));
-                    Debug.Log("in right");
-                    break;
-
-                case "rotateLeftBlock(Clone)":
-                    StartCoroutine(script.RotateAround(Vector3.up, -90.0f, 1.0f));
-                    Debug.Log("in left");
-                    break;
-
-                case "moveBlock(Clone)":
-                    //moveForward();
-                    script.Move();
-                    Debug.Log("in move");
-                    break;
-            }
-        }*/
     }
 }
