585e279ec31cfa80bcd8d137a08e69036757f25d
Raneem Dewaib <r.dewaib95@gmail.com>
2018-04-21 01:35:56 +0200

    display level 1 Helping Messages (Done)

--- Assets/Scripts/MessagesController.cs
+++ Assets/Scripts/MessagesController.cs
@@ -4,60 +4,99 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class MessagesController : MonoBehaviour {
-    //Dictionary<int, string> messages = new Dictionary<int, string>();
-    List<string> messages = new List<string>();
+    List<string> messagesLevel1 = new List<string>();
 
     GameObject image;
     GameObject nextButton;
     GameObject previousButton;
+    GameObject doneButton;
 
     int nextButtonFlag = 0;
-    
+    int previousButtonFlag = 0;
 
-
-    Text message1;
-    string message2;
-    string message3, message4;
+    int i = 0; //counter to display messages when next is pressed
 
     // Use this for initialization
     void Start () {
-        messages.Add("Level1Message1");
-        messages.Add("Level1Message2");
+        messagesLevel1.Add("Level1Message1");
+        messagesLevel1.Add("Level1Message2");
+        messagesLevel1.Add("MessagesLevel1-03");
+        messagesLevel1.Add("MessagesLevel1-04");
+        messagesLevel1.Add("MessagesLevel1-05");
+        messagesLevel1.Add("MessagesLevel1-06");
+        messagesLevel1.Add("MessagesLevel1-07");
+        messagesLevel1.Add("MessagesLevel1-08");
+        messagesLevel1.Add("MessagesLevel1-09");
 
         image = GameObject.Find("Image");
+        nextButton = image.transform.Find("Next Button").gameObject;
+        previousButton = image.transform.Find("Previous Button").gameObject;
+        doneButton = image.transform.Find("Done Button").gameObject;
 
-        //message2 = GameObject.Find("text2").GetComponentInChildren<Text>();
-        //message3 = GameObject.Find("text3").GetComponentInChildren<Text>();
-
-        //message2.SetActive(false);
-        //message3.SetActive(false);
+        doneButton.SetActive(false);
+        previousButton.SetActive(false);
     }
 
     // Update is called once per frame
     void Update () {
-        //updateButton();
-        nextButton = image.transform.Find("Next Button").gameObject;
+        
+        nextButton.GetComponent<Button>().onClick.AddListener(delegate { updateNextButton(); }); 
+        previousButton.GetComponent<Button>().onClick.AddListener(delegate { updatePreviousButton(); });
 
-        nextButton.GetComponent<Button>().onClick.AddListener(delegate { StartCoroutine(updateButton()); });
-
-        //image.GetComponent<Button>().onClick.AddListener(delegate { updateButton(); });
-	}
-   
-    private IEnumerator updateButton()
+        Sprite mySprite = Resources.Load<Sprite>(messagesLevel1[i]);
+        image.GetComponent<Image>().sprite = mySprite;
+    }
+    
+    private void updateNextButton ()
     {
-        for(int i = 0; i < messages.Count; i++)
+        if (i < messagesLevel1.Count && NextButton.clicked == true && nextButtonFlag == 0)
         {
-            if(nextButtonFlag == 0 && NextButton.clicked == true)
+            previousButton.SetActive(true);
+
+            Debug.Log(i);
+            nextButtonFlag = 1;
+            nextButtonFlag = 0;
+            NextButton.clicked = false;
+            i++;
+
+            //begining of the messages
+            if (i == 0)
             {
-                nextButtonFlag = 1;
-                Sprite mySprite = Resources.Load<Sprite>(messages[i]);
-                image.GetComponent<Image>().sprite = mySprite;
-                nextButtonFlag = 0;
+                previousButton.SetActive(false);
             }
- 
-            yield return new WaitForSeconds(2.0f);
-            
+
+            //reached the end of the messages
+            if (i == messagesLevel1.Count - 1)
+            {
+                nextButton.SetActive(false);
+                doneButton.SetActive(true);
+            }
+        }    
+   }
+
+    private void updatePreviousButton ()
+    {
+        if (i > 0 && PreviousButton.clicked == true && previousButtonFlag == 0)
+        {
+            Debug.Log(i);
+            previousButtonFlag = 1;
+            previousButtonFlag = 0;
+            PreviousButton.clicked = false;
+            i--;
+        }
+
+        //begining of the messages
+        if (i == 0)
+        {
+            previousButton.SetActive(false);
+        }
+
+        //pressed previous when player reached the end of the messages
+        if (i == messagesLevel1.Count - 2)
+        {
+            nextButton.SetActive(true);
+            doneButton.SetActive(false);
         }
     }
-    //NextButton.clicked == false;
+
 }
---
+++ Assets/Scripts/PreviousButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreviousButton : MonoBehaviour {
+
+    public static bool clicked = false;
+
+    public void previousButtonClicked()
+    {
+        clicked = true;
+    }
+}
