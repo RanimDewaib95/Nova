cf533a50cfda8b5536d3cfe9cdf0cbe852540bc6
Heba ElNashar <honeyantaka@yahoo.com>
2018-04-21 18:25:41 +0200

    add on collision function and import grab button

---
+++ Assets/Scripts/Player Controller/CollectableHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableHandler : MonoBehaviour {
+
+    public int score = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log("in collectible handler script");
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("in collision function");
+        if (col.gameObject.tag == "collectible")
+        {
+            score += 1;
+            Debug.Log(score);
+            Destroy(col.gameObject);
+        }
+    }
+}
