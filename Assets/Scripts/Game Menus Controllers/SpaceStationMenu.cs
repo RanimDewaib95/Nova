8ab3eb3c493b192ba86cf73d7e7ef366108fb24e
Raneem Dewaib <r.dewaib95@gmail.com>
2018-04-23 20:06:49 +0200

    add minor fixes

---
+++ Assets/Scripts/Game Menus Controllers/SpaceStationMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpaceStationMenu : MonoBehaviour {
+
+    public void levelOneClicked()
+    {
+        SceneManager.LoadScene("Spacestation-Level1");
+    }
+}
