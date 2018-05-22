5a8498b77b5382b9ce4d6e415119c2313df274dd
Raneem Dewaib <r.dewaib95@gmail.com>
2018-05-22 10:48:11 +0200

    fix minor issues

--- Assets/Scripts/Game Menus Controllers/LevelsMenuController.cs
+++ Assets/Scripts/Game Menus Controllers/LevelsMenuController.cs
@@ -7,12 +7,22 @@ public class LevelsMenuController : MonoBehaviour {
 
     public void spaceStationButtonClicked()
     {
-        SceneManager.LoadScene("Levels Menu Intro");
+        SceneManager.LoadScene("Spacestation-Level1");
     }
 
     public void planet1ButtonClicked()
     {
-        SceneManager.LoadScene("Planet1-Level 1");
+        SceneManager.LoadScene("Planet1-Level1");
+    }
+
+    public void planet3ButtonClicked()
+    {
+        SceneManager.LoadScene("Planet2-Level1");
+    }
+
+    public void planet4ButtonClicked()
+    {
+        SceneManager.LoadScene("Planet3-Level1");
     }
 
     public void backButtonClicked()
